e60b96608832e1f633900bcb52d3fc5a9465fdf9
Joel <joelmp3@live.com>
2023-09-24 18:20:18 -0600

    Fixed issue with 1 value on empty text

--- src/JS-Modules/wordCounter.js
+++ src/JS-Modules/wordCounter.js
@@ -8,7 +8,7 @@ export default class WordCounter{
         this.contador.addEventListener('input', () => {
             let value = this.contador.value.trim();
             this.__chars = value.length;
-            this.__words = value.split(' ').length;
+            this.__words = value === '' ? 0 : value.split(' ').length;
 
             this.word_counter.innerHTML = this.__words;
             this.char_counter.innerHTML = this.__chars;
