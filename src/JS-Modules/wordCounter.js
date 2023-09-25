81f2c9b74e3b43f99be2624ecf795f941245097a
Joel <joelmp3@live.com>
2023-09-24 18:28:01 -0600

    Added copy feature

--- src/JS-Modules/wordCounter.js
+++ src/JS-Modules/wordCounter.js
@@ -1,5 +1,7 @@
 export default class WordCounter{
     constructor(){
+        this.__chars = 0;
+        this.__words = 0;
         this.domLoader();
         this.events();        
     }
@@ -14,11 +16,19 @@ export default class WordCounter{
             this.char_counter.innerHTML = this.__chars;
 
         });
+        this.word_btn.addEventListener('click', () => {
+            navigator.clipboard.writeText(this.__words);
+        });
+        this.char_btn.addEventListener('click', () => {
+            navigator.clipboard.writeText(this.__chars);
+        });
     }
 
     domLoader(){
         this.contador = document.querySelector('#wordCounter-app');
         this.word_counter = document.querySelector('#wordCount');
         this.char_counter = document.querySelector('#charCount');
+        this.word_btn = document.querySelector('#copyWordsBtn');
+        this.char_btn = document.querySelector('#copyCharsBtn');
     }
 }
